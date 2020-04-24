ba604250e3ec92a4e39d862d62849091b1e20129
Marcos Luna <ml042685@yahoo.com>
2020-04-23 22:15:48 -0500

    var

---
+++ Hangman.js
@@ -0,0 +1,10 @@
+var wordOptions = ["a nightmare on elm street", "the breakfast club", "top gun", "back to the future", "coming to america", "sixteen candles", "the princess bride", "the goonies", "stand by me", "trading places", "blade runner", "the karate kid" ];
+var selectWord = "";
+var lettersinWord = [];
+var numBlanks = 0;
+var blanksAndSuccess = [];
+var wrongLetters = [];
+
+var winCount = 0;
+var lossCount = 0;
+var guessesLeft = 0;
